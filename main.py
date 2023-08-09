94ecea6e9f97f2c674708f9cfaa4a6fa07a65534
D1288 <107957273+D1288@users.noreply.github.com>
2023-08-09 16:45:15 -0700

    Added functions to check for winner and stop game if one is detected

--- main.py
+++ main.py
@@ -1,36 +1,74 @@
 board = [ ["-", "-", "-"],
-         ["-", "-", "-"],
-         ["-", "-", "-"] ]
-print(board[0])
-print(board[1])
-print(board[2])
+          ["-", "-", "-"],
+          ["-", "-", "-"] ]
+ 
+def printBoard(board):
+   print("\n")
+   print("\t     |     |")
+   print("\t  {}  |  {}  |  {}".format(board[0][0], board[0][1], board[0][2]))
+   print('\t_____|_____|_____')
+   print("\t     |     |")
+   print("\t  {}  |  {}  |  {}".format(board[1][0], board[1][1], board[1][2]))
+   print('\t_____|_____|_____')
+   print("\t     |     |")
+   print("\t  {}  |  {}  |  {}".format(board[2][0], board[2][1], board[2][2]))
+   print("\t     |     |")
+   print("\n")
+
+def checkWinner(currPlayer, board):
+  for row in range(0,3):
+    if board[row][0]==board[row][2] and board[row][0] != '-':
+      if board[row][0]==currPlayer:
+        print('{} is winner'.format(currPlayer))
+        return True
+  for col in range(0,3):
+    if board[0][col]==board[1][col]==board[2][col] and board[0][col] != '-':
+      if board[0][col]==currPlayer:
+        print('{} is winner'.format(currPlayer))
+        return True
+  if board[0][0]==board[1][1]==board[2][2] and board[0][0] != '-':
+    if board[0][0]==currPlayer:
+      print('{} is winner'.format(currPlayer))
+      return True
+  if board[0][2]==board[1][1]==board[2][0] and board[0][2] != '-':
+    if board[0][2]==currPlayer:
+      print('{} is winner'.format(currPlayer))
+      return True
+  return False
+  
+printBoard(board)
 col=0
 row=0
 playerTurn = "X"
+ 
 for counter in range(1,10):
-   valid = False
-   while (valid == False): #here is the while loop that addresses Item 2.
-     col=0
-     row=0
-     while (col < 1 or col > 3):
-       col = int(input(playerTurn + " player, select a column 1-3: "))
-       if (col < 1 or col > 3):
-         print("The column must be between 1 and 3.")
-     while (row < 1 or row > 3):
-       row = int(input(playerTurn + " player, select a row 1-3: "))
-       if (row < 1 or row > 3):
-         print("The row must be between 1 and 3.")
-     col -= 1
-     row -= 1
-     if board[row][col] == '-':
-       board[row][col] = playerTurn
-       valid = True #here is the break statement that addresses Item 2
-     else:
-       print("Oops, that spot was already taken. Please select another spot.")
-   print(board[0])
-   print(board[1])
-   print(board[2])
-   if playerTurn =="X":
-     playerTurn="O"
-   else:
-     playerTurn="X"
+
+    validMove = False
+    while (validMove == False):
+      col=0
+      row=0
+      while (col < 1 or col > 3):
+        col = int(input(playerTurn + " player, select a column 1-3: "))
+        if (col < 1 or col > 3):
+          print("The column must be between 1 and 3.")
+      while (row < 1 or row > 3):
+        row = int(input(playerTurn + " player, select a row 1-3: "))
+        if (row < 1 or row > 3):
+          print("The row must be between 1 and 3.")
+      col -= 1
+      row -= 1
+      if board[row][col] == '-':
+        board[row][col] = playerTurn
+        validMove=True;
+      else:
+        print("Oops, that spot was already taken. Please select another spot.")
+ 
+    printBoard(board)
+    if(checkWinner(playerTurn,board)):
+      break
+ 
+    if playerTurn =="X":
+      playerTurn="O"
+    else:
+      playerTurn="X"
+
